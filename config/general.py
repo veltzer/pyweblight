686e9c61d51bffd8562ab73fdfad29ab2ae160a3
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-04 05:04:18 +0300

    new template

--- config/general.py
+++ config/general.py
@@ -1,12 +1,11 @@
 import datetime
 import os
 import socket
-
-from config.helpers import help_check_output
+import subprocess
 
 general_current_folder = os.path.basename(os.getcwd())
 general_current_year = datetime.datetime.now().year
-general_homedir = os.path.expanduser('~')
+general_homedir = os.path.expanduser("~")
 # d.general_hostname=subprocess.check_output(['hostname']).decode().rstrip()
 general_hostname = socket.gethostname()
-general_domain_name = help_check_output(['hostname', '--domain']).rstrip()
+general_domain_name = subprocess.check_output(["hostname", "--domain"]).decode().rstrip()
