59b670783ad98660252fa01f185fadd0a505cf60
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:12:44 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -3,39 +3,30 @@ import config.project
 package_name = config.project.project_name
 
 console_scripts = [
-    'pyweblight=pyweblight.main:main',
-]
-
-setup_requires = [
+    "pyweblight=pyweblight.main:main",
 ]
 
 run_requires = [
-    'python-daemon',
-    'pytconf',
-    'pylogconf',
+    "python-daemon",
+    "pytconf",
+    "pylogconf",
 ]
 
 test_requires = [
-    'pylint',
-    'pytest',
-    'pytest-cov',
-    'pyflakes',
-    'flake8',
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "pyflakes",
+    "flake8",
 ]
 
 dev_requires = [
-    'pyclassifiers',
-    'pypitools',
-    'pydmt',
-    'Sphinx',
-    'pymakehelper',
+    "pyclassifiers",
+    "pypitools",
+    "pydmt",
+    "Sphinx",
+    "pymakehelper",
 ]
-
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {
-}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
