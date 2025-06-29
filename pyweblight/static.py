d3d0c2e72b0f78e21e77db5fc9c2f7bbecfc1c18
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 09:34:03 +0300

    fixed up deps

--- pyweblight/static.py
+++
@@ -1,5 +0,0 @@
-""" version which can be consumed from within the module """
-VERSION_STR = "0.0.6"
-DESCRIPTION = "Pyweblight is a small configurable web server for developers"
-APP_NAME = "pyweblight"
-LOGGER_NAME = "pyweblight"
