724a191675ec36012ec3a0f81a509c88149c94aa
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-06 12:09:23 +0300

    pydmt -> rsconstruct

---
+++ config/project.lua
@@ -0,0 +1,11 @@
+NAME = "pyweblight"
+DESCRIPTION_SHORT = "Pyweblight is a small configurable web server for developers"
+KEYWORDS = {
+    "http",
+    "https",
+    "apache",
+    "python",
+    "python3",
+    "server",
+    "web",
+}
